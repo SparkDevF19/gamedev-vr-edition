b6968766a9c6ab8c3817910a21b87c73b2a93f24
rivash728 <35610411+rivash728@users.noreply.github.com>
2019-11-20 14:41:17 -0500

    Display UI Updated

--- Vive VR/Assets/DisplayInventory.cs
+++ Vive VR/Assets/DisplayInventory.cs
@@ -22,32 +22,42 @@ public class DisplayInventory : MonoBehaviour
 
     private void Update()
     {
-        //UpdateDisplay();
+        UpdateDisplay();
     }
 
     public void UpdateDisplay()
     {
         for (int i = 0; i < inventory.Container.Count; i++)
         {
-            if(itemDisplayed.ContainsKey(inventory.Container[i]))
+            if (itemDisplayed.ContainsKey(inventory.Container[i]))
             {
-                // itemDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>.text = inventory.Container
+                itemDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            }
+            else
+            {
+                var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+                itemDisplayed.Add(inventory.Container[i], obj);
             }
         }
     }
 
     public void CreateDisplay()
     {
-        for( int i = 0; i < inventory.Container.Count; i++)
+        for (int i = 0; i < inventory.Container.Count; i++)
         {
             var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
             obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            itemDisplayed.Add(inventory.Container[i], obj);
+
         }
     }
 
     public Vector3 GetPosition(int i)
     {
-        return new Vector3(X_START + ( X_SPACE_BETWEEN_ITEM * ( i % NUMBER_OF_COLUMN)), Y_START + (-Y_SPACE_BETWEEN_ITEMS * (i/NUMBER_OF_COLUMN)), 0f);
+        return new Vector3(X_START + (X_SPACE_BETWEEN_ITEM * (i % NUMBER_OF_COLUMN)), Y_START + (-Y_SPACE_BETWEEN_ITEMS * (i / NUMBER_OF_COLUMN)), 0f);
     }
 }
+
